1dd3c9cf3c5259d836ff6c3c4a4332c74a5291ed
peoplewhite <shonshon7@gmail.com>
2015-06-18 18:06:20 +0800

    homework done

--- Homework_ac_150617/CollectionViewController.m
+++ Homework_ac_150617/CollectionViewController.m
@@ -13,6 +13,10 @@
     NSArray *arrPic;
     NSIndexPath *clickedIndexPath;
     int clickedNum;
+    
+    NSMutableArray *arrDownloadPic;
+    NSArray *arrPicURL;
+    
 //    UITapGestureRecognizer *tapGR;
 }
 
@@ -44,7 +48,30 @@ static NSString * const reuseIdentifier = @"Cell";
 //    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:reuseIdentifier];
 //    [self.collectionView registerNib:[UINib nibWithNibName:@"CollectionViewCell" bundle:nil] forCellWithReuseIdentifier:reuseIdentifier];
  
-//     Do any additional setup after loading the view.
+    
+    
+    //Download pics from URL
+    NSString *result = [self httpGet:@"http://www.lovistw.com/poda.json"];
+//    NSLog(@"%@", result);
+    NSError *e = nil;
+    NSData *jsonData = [result dataUsingEncoding:NSUTF8StringEncoding];
+    
+    NSMutableArray *arr = [NSJSONSerialization JSONObjectWithData:jsonData options:NSJSONReadingMutableContainers error:&e];
+    
+//    int i = 0;
+    arrDownloadPic = [[NSMutableArray alloc]init];
+    for (NSMutableDictionary *dictionary in arr) {
+//    NSLog(@"%i", i);
+//        arrPicURL[i] = dictionary[@"imgPath"];
+//        NSString *str = @"http://www.finalfantasyviipc.com/images/media_cloud_big.jpg";
+        NSURL *gurl = [NSURL URLWithString:dictionary[@"imgPath"]];
+        NSData *data = [NSData dataWithContentsOfURL:gurl];
+        [arrDownloadPic addObject:[UIImage imageWithData:data]];
+//        arrDownloadPic[i] = [UIImage imageWithData:data];
+//        i++;
+//        NSLog(@"count = %i", arrDownloadPic.count);
+    }
+    NSLog(@"Load Pic done");
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -52,25 +79,21 @@ static NSString * const reuseIdentifier = @"Cell";
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
-- (void)showNum {
-    
-    NSLog(@"Hello    %i", clickedIndexPath.row);
-    
-}
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+
 
 #pragma mark <UICollectionViewDataSource>
+
+-(NSString *)httpGet:(NSString *)urllink{
+    NSURL *url = [NSURL URLWithString:urllink];
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc]initWithURL:url cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:10];
+    [request setHTTPMethod:@"GET"];
+    NSData *received = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+    NSString *str1 = [[NSString alloc]initWithData:received encoding:NSUTF8StringEncoding];
+    return str1;
+}
 
 - (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
     return 1;
@@ -78,13 +101,16 @@ static NSString * const reuseIdentifier = @"Cell";
 
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-    return [arrPic count];
+//    return [arrPic count];
+//    NSLog(@"%i", arrDownloadPic.count);
+    return arrDownloadPic.count;
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
     UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:reuseIdentifier forIndexPath:indexPath];
     UIImageView *imageView = (UIImageView *)[cell viewWithTag:100];
-    imageView.image = arrPic[indexPath.row];
+//    imageView.image = arrPic[indexPath.row];
+    imageView.image = arrDownloadPic[indexPath.row];
 //    imageView.image = arrPic[0];
     
     clickedIndexPath = indexPath;
@@ -106,7 +132,8 @@ static NSString * const reuseIdentifier = @"Cell";
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     DetailViewController *detailVC = [segue destinationViewController];
-    detailVC.imageClicked = arrPic[clickedNum];
+//    detailVC.imageClicked = arrPic[clickedNum];
+    detailVC.imageClicked = arrDownloadPic[clickedNum];
 }
 
 #pragma mark <UICollectionViewDelegate>
